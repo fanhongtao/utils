1ad3bd28d0583799e4bb1ba810a7bbcb23ef6d0f
Fan Hongtao <fanhongtao@gmail.com>
2020-01-30 11:19:13 +0800

    启动wiki时，使用 config.rb

--- start_wiki.sh
+++ start_wiki.sh
@@ -29,11 +29,12 @@ else
     port=$2
 fi
 
+# 使用了 $wiki_path 目录下的 config.rb
 cd $wiki_path
 if [ $# -gt 2 ]; then
-  nohup gollum --mathjax --port $port --allow-uploads page --adapter rugged --js --css -b $3 $wiki_path &
+  nohup gollum --mathjax --port $port --allow-uploads page --adapter rugged --js --css -c config.rb -b $3 $wiki_path &
 else
-  nohup gollum --mathjax --port $port --allow-uploads page --adapter rugged --js --css $wiki_path &
+  nohup gollum --mathjax --port $port --allow-uploads page --adapter rugged --js --css -c config.rb $wiki_path &
 fi
 
 exit 0
