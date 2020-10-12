fc43ce9dfbdc8a976a679b663ebfcc63b8cf3d47
Fan Hongtao <fanhongtao@gmail.com>
2020-10-12 19:38:50 +0800

    Delete arg for 'base_path'.

--- docker/gollum/create_wiki_container.sh
+++ docker/gollum/create_wiki_container.sh
@@ -5,12 +5,11 @@
 function ShowHelp()
 {
     echo "Usage:"
-    echo "    create_wiki_container.sh container_name wiki_path  [port [base_path [other_args] ]]"
+    echo "    create_wiki_container.sh container_name wiki_path  [port [other_args] ]"
     echo "Examples:"
     echo "    create_wiki_container.sh study ~/wiki/study"
     echo "    create_wiki_container.sh wiki_temp ~/wiki/temp  4568"
-    echo "    create_wiki_container.sh wiki_temp ~/wiki/temp  4568  /temp"
-	echo "    create_wiki_container.sh wiki_temp ~/wiki/temp  4568  /temp  --no-edit"
+    echo "    create_wiki_container.sh wiki_temp ~/wiki/temp  4568  -b /temp --no-edit"
 }
 
 
@@ -31,15 +30,8 @@ else
     port=4567  # use default port 4567
 fi
 
-if [ $# -gt 0 ]; then
-  base_path="-b $1"
-  shift
-else
-  base_path=""
-fi
-
 cd $wiki_path
-docker create --name $container_name -i -t -p $port:4567 -v "$PWD":/wiki fanhongtao/gollum-alpine --allow-uploads dir $base_path $*
+docker create --name $container_name -i -t -p $port:4567 -v "$PWD":/wiki fanhongtao/gollum-alpine --allow-uploads dir $*
 
 docker start $container_name
 
