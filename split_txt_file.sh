eeaa167ee43e293c574652546540b57c4df26e1d
Fan Hongtao <fanhongtao@gmail.com>
2021-01-28 17:06:53 +0800

    Rename the splited file

--- split_txt_file.sh
+++ split_txt_file.sh
@@ -34,4 +34,18 @@ done
 
 prefix=`basename $txt_file .txt`
 suffix="-%02d.txt"
+
+# 删除分析后的文件（因为有改名，所以要先删除旧的文件）
+rm "$prefix-"*
+
+# 分析文件
 csplit --prefix=$prefix --suffix-format=$suffix  $txt_file  $args
+
+# 修改分拆后的文件名。将编号由 00 开始，转换成由 01 开始。
+count=`ls "$prefix-"* | wc -l`
+for file in `ls "$prefix-"* | sort -r` ; do
+    newName="${prefix}-"`printf "%02d" ${count}`".txt"
+    echo "mv $file to $newName"
+    mv $file $newName
+    let count=count-1
+done
