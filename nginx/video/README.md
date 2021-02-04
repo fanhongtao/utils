69f7f337e01dc8d1311ca948e94300cac46887d8
Fan Hongtao <fanhongtao@gmail.com>
2021-02-04 21:24:33 +0800

    add: video

---
+++ nginx/video/README.md
@@ -0,0 +1,5 @@
+# 功能说明
+
+本目录下的文件，可在 nginx 搭建简易的视频网站时使用。
+
+* [create_index.py](create_index.py) : 扫描目录下所有视频文件，创建 index.html 文件。
---
+++ nginx/video/create_index.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# Generate INDEX.HTML
+
+
+import pathlib
+import sys
+
+
+def show_help():
+    """ Show help message """
+    program = sys.argv[0]
+    print("Usage:" + program + " root-path [video-suffix-list]")
+    print("Example:")
+    print("    " + program + "/home/video  mp4 flv")
+
+
+def match(name, suffix_list):
+    for one in suffix_list:
+        if name.endswith(one):
+            return True
+    return False
+
+
+def main():
+    if len(sys.argv) == 1:
+        show_help()
+        return
+
+    root = sys.argv[1]
+    if len(sys.argv) > 2:
+        suffix_list = sys.argv[2:]
+    else:
+        suffix_list = ["mp4", "flv", "rmvb", "mkv"]
+
+    header = ('<html>\n'
+              '<head>\n'
+              '<meta http-equiv="content-type" content="text/html; charset=utf-8">\n'
+              '<title>Videos</title>\n'
+              '</head>\n'
+              '<body >\n'
+              '<ul>\n')
+    tailer = '</ul>\n<body>\n</html>\n'
+    with open("index.html", mode="w") as file:
+        file.write(header)
+        directory = pathlib.Path(root)
+        for path in sorted(directory.rglob('*')):
+            if path.is_file():
+                if match(path.name.lower(), suffix_list):
+                    file.write("<li><a href='" + str(path) +
+                               "'>" + path.name + "</a></li>\n")
+        file.write(tailer)
+
+
+if __name__ == '__main__':
+    main()
